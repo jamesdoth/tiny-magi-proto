bc06b4483a3c06fc94329516cee9b06c6461463b
jamesdoth <jamesdoth@gmail.com>
2023-08-11 19:32:25 -0700

    movement implemented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerStateMachine StateMachine {  get; private set; }
+    public PlayerMoveState MoveState { get; private set; }
 
     [SerializeField]
     private PlayerData playerData;
@@ -12,34 +13,44 @@ public class Player : MonoBehaviour
     public PlayerInputHandler InputHandler { get; private set; }
     public Rigidbody2D RB { get; private set; }
     private Vector2 workspace;
+    public Vector2 CurrentVelocity { get; private set; }
 
     private void Awake()
     {
         StateMachine = new PlayerStateMachine();
+        MoveState = new PlayerMoveState(this, StateMachine, playerData);
     }
 
     private void Start()
     {
-        
+        InputHandler = GetComponent<PlayerInputHandler>();
+        RB = GetComponent<Rigidbody2D>();
+
+        StateMachine.Initialize(MoveState);
     }
 
     private void Update()
     {
-        
+        CurrentVelocity = RB.velocity;
+        StateMachine.CurrentState.LogicUpdate();
     }
 
     private void FixedUpdate()
     {
-        
+        StateMachine.CurrentState.PhysicsUpdate();
     }
 
     public void SetVelocityX(float velocity)
     {
-
+        workspace.Set(velocity, CurrentVelocity.y);
+        RB.velocity = workspace;
+        CurrentVelocity = workspace;
     }
 
     public void SetVelocityY(float velocity)
     {
-
+        workspace.Set(CurrentVelocity.x, velocity);
+        RB.velocity = workspace;
+        CurrentVelocity = workspace;
     }
 }
--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
@@ -11,6 +10,7 @@ public class PlayerInputHandler : MonoBehaviour
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         RawMovementInput = context.ReadValue<Vector2>();
+
         NormInputX = (int)(RawMovementInput * Vector2.right).normalized.x;
         NormInputY = (int)(RawMovementInput * Vector2.up).normalized.y;
     }
